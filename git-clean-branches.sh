ac9cfc77555b4b06cab12cf41fd3cea8485fdfbd
Ranjith Muddana <25493833+ranjithmuddana@users.noreply.github.com>
2020-07-03 18:56:15 -0400

    Update git-clean-branches.sh

--- git-clean-branches.sh
+++ git-clean-branches.sh
@@ -1,15 +1,17 @@
-for branch in `git branch -r --merged | grep -v test | grep -v upstream | grep -v HEAD `; 
+## Usage : ./git-clean-branches.sh 2019-01-01 
+## Tells whether the branch can be deleted or needs to present
+for branch in `git branch -r --merged | egrep -v "dev*|mas*|HE*" `; 
 do 
-echo 'START ################################'
-echo "$(git log $branch -n 1 --after=$1)"
+	echo 'START ################################'
+	#Uncomment below line to see commit details
+	#echo "$(git log $branch -n 1 --after=$1)"
 	#echo -e `git show --format="%aN\t %m %ae\t %cD\t %cr\t" $branch | head -n 1` \\t$branch;
 	if [ -n "$(git log $branch -n 1 --after=$1)" ]; then
-		echo "TO BE MIGRATED: $branch"	
+		echo "TO BE KEPT: $branch"	
 	else
 		echo "TO BE DELETED: $branch"
 	fi
-echo "######################################"
-                echo "#####################################"
-                echo 'ENDED ################################'
+	echo 'ENDED ################################'
+	echo ''
 done
 
